bddc647b7f07deda50ecb75865dafbcde7b71ccb
jzhnice <1749597552@qq.com>
2021-04-20 17:29:19 +0800

    Initial commit

---
+++ src/BookShop/book.java
@@ -0,0 +1,14 @@
+package BookShop;
+
+/**
+ * @version 1.0
+ * @author: jiazhihao
+ * @date: 2021-04-10 13:40
+ */
+public class book {
+    String[] name =  new String[30];   //数组1存储图书名称数组
+    int [] state = new int[30];        //数组2存储图书借出状态：0已借出/1可借
+    String[] date = new String[30];    //数组3存储图书借出日期
+    int[] count = new int[30];//借出次数
+
+}
---
+++ src/BookShop/csshi.java
@@ -0,0 +1,15 @@
+package BookShop;
+
+/**
+ * @version 1.0
+ * @author: jiazhihao
+ * @date: 2021-04-10 13:40
+ */
+public class csshi {
+    public static void main(String[] args) {
+        shop sp = new shop();
+        sp.initial();
+        sp.kaishi();
+    }
+}
+
